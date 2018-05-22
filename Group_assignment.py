6c5447f1896ecb09e9065f25a00902c5c3e9a10d
Sorin Park <14sp24@queensu.ca>
2018-05-22 13:14:05 -0400

    new file

--- Group_assignment.py
+++ Group_assignment.py
@@ -1,22 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 22 11:39:13 2018
 
-@author: Andrii Trokoz
-"""
-from collections import Counter
-with open('./new_file.txt', 'r+') as text:
-    read_text = text.read().strip().split()
+with open ('textfile.txt', 'r+') as text:
+    text_read = text.read().lower().strip()
+    word_read = text_read.split()
 
-    
-    new_list = []
-    
-    for i in read_text:
-            new_list.append(i.lower())
-    
-    print(Counter(new_list))
+    wordcount = {}
+    for word in word_read:
+        if word.isalpha():
             
-         
-    
-    
-    
+            if word not in wordcount:
+                wordcount[word] = 1
+            else: 
+                wordcount[word] += 1
+    for key,count in wordcount.items():
+        print("%s: %d" %(key, count))
